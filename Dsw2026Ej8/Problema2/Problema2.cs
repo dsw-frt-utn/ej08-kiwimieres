75a0dccce06db61ed536189a4ad3c55885ac36ba
Sebastian Mieres <sebassegundomieres@gmail.com>
2026-05-01 15:47:29 -0300

    Se resuelve el problema 2

---
+++ Dsw2026Ej8/Problema2/Problema2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema2
+{
+    public class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            var Total = quantity * unitPrice;
+            if (quantity <= 0m) { Total = 0m; }
+            long Code = productCode;
+            string Description = productDescription;
+            int Quantity = quantity;
+
+            var claseAnonima = new { Code, Description, Quantity, Total };
+
+            return $"{claseAnonima.Code}-{claseAnonima.Description}-{claseAnonima.Total}";
+        }
+
+    }
+}
