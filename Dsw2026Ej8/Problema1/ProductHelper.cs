7d556c6b30758874a244bfe84d027d29bc7ef598
Sebastian Mieres <sebassegundomieres@gmail.com>
2026-05-01 15:29:46 -0300

    Se resuelve el problema 1

---
+++ Dsw2026Ej8/Problema1/ProductHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema1
+{
+    internal partial class ProductHelper
+    {
+        public string ObtenerEtiquetaProducto(long code, string description, decimal price)
+        {
+            stockPapa -= 1; 
+            return $"[{code}] {description} - {price:C2}";
+        }
+
+    }
+}
---
+++ Dsw2026Ej8/Problema1/ProductHelperComplemento.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema1
+{
+    internal partial class ProductHelper
+    {
+        int stockPapa = 100;
+    }
+}
