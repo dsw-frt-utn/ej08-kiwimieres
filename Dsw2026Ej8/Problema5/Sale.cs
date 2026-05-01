b945c31deded7a30b5988119700a61f509a3ec94
Sebastian Mieres <sebassegundomieres@gmail.com>
2026-05-01 16:43:55 -0300

    Se resuelve el problema 5

---
+++ Dsw2026Ej8/Problema5/Sale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema5
+{
+    public class Sale
+    {
+        public decimal ImporteBase { get; set; }
+        public virtual decimal CalculateTotal()
+        {
+            return ImporteBase;
+        }
+    }
+}
---
+++ Dsw2026Ej8/Problema5/WholesaleSale.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema5
+{
+    public class WholesaleSale : Sale
+    {
+          public override decimal CalculateTotal()
+        {
+            decimal total = base.CalculateTotal();
+            
+                total *= 0.9m; // Aplicar un descuento del 10%
+            
+            return total;
+        }
+    }
+}
