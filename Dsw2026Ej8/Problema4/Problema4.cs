f037ed75404e7c61499f283c6e62517e94a544d4
Sebastian Mieres <sebassegundomieres@gmail.com>
2026-05-01 16:29:32 -0300

    Se resuelve el problema 4

---
+++ Dsw2026Ej8/Problema4/Problema4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema4
+{
+    internal class Problema4
+    {
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            int suma = 0;
+            int cantidadNotas = 0;
+            if(nota1.HasValue && nota1.Value >= 0 && nota1.Value <= 10)
+            {
+                suma += nota1.Value;
+                cantidadNotas++;
+            }
+            if(nota2.HasValue && nota2.Value >= 0 && nota2.Value <= 10)
+            {
+                suma += nota2.Value;
+                cantidadNotas++;
+            }
+            if(nota3.HasValue && nota3.Value >= 0 && nota3.Value <= 10)
+            {
+                suma += nota3.Value;
+                cantidadNotas++;
+            }
+
+            return cantidadNotas > 0 ? suma / cantidadNotas : 0;
+        }
+
+    }
+}
