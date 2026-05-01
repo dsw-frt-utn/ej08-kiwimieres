e1833a9ebf89a2a4eee58e4b17b7a090482607de
Sebastian Mieres <sebassegundomieres@gmail.com>
2026-05-01 16:13:13 -0300

    Se resuelve el problema 3, tuve que hacer la clase Product porque no estaba en el repo

---
+++ Dsw2026Ej8/Problema3/Problema3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema3
+{
+    public class Problema3
+    {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int variableLocal = originalValue;
+            variableLocal++;
+            Product copiaProduct = product;
+            copiaProduct.SetDescription("Cambio de descripcion");
+
+            return $"{originalValue}-{variableLocal}-{copiaProduct.Description}";
+        }
+    }
+}
---
+++ Dsw2026Ej8/Problema3/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema3
+{
+    internal class Product
+    {
+        public string Description { get; private set; }
+        public Product(string initialDescription)
+        {
+            Description = initialDescription;
+        }
+        public void SetDescription(string newDescription)
+        {
+            Description = newDescription;
+        }
+    }
+}
