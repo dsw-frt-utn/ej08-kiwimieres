92e967429ea8ed49358c8cc1e8d295d66300bf0f
Sebastian Mieres <sebassegundomieres@gmail.com>
2026-05-01 16:55:17 -0300

    Se resuelve el problema 6

---
+++ Dsw2026Ej8/Problema6/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema6
+{
+//    el método de extensión debe:
+//○ eliminar espacios al inicio y al final,
+//○ convertir el texto a mayúsculas,
+//○ reemplazar espacios internos por guiones.
+//● Si el código es null, debe retornar "SIN-CODIGO".
+
+    public static class Extensions
+    {
+        public static string ToProductCode(this string str)
+        {
+            if (str == null) return "SIN-CODIGO";
+            return str.Trim().ToUpper().Replace("", "-");
+        }
+    }
+}
